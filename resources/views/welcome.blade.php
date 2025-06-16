15b3efbe9cdf1e272fbc1102a0250eef9affc567
brunocriacoes <br.rafael@outlook.com>
2025-06-16 10:19:09 -0300

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -49,7 +49,7 @@
         const swiper = new Swiper(".mySwiper", {
             loop: true,
             autoplay: {
-                delay: 2500,
+                delay: 5000,
                 disableOnInteraction: false,
             },
         });
